62c8dbd02be6e8fed7b1a6ac0c41819ffa33d703
Dhruv Rajan <dhruv@krishnaprem.com>
2016-01-25 01:11:38 -0800

    Fixed flywheel.py to use the robot_vision properties correctly

--- py/grt/mechanism/flywheel.py
+++ py/grt/mechanism/flywheel.py
@@ -1,19 +1,19 @@
 from wpilib import CANTalon
 from grt.core import Sensor
 
-class Flywheel:
 
+class Flywheel:
     STANDBY_SPEED = -8400
 
-    def __init__(self, shooter):
-        self.shooter = shooter
-        self.flywheel_motor = shooter.flywheel_motor
-        self.robot_vision = shooter.robot_vision
+    def __init__(self, robot_vision, flywheel_motor):
+        self.flywheel_motor = flywheel_motor
+        self.robot_vision = robot_vision
         self.currentspeed = self.STANDBY_SPEED
 
     def spin_to_target_speed(self):
-        if self.robot_vision.get_target_view():
-            self.rpm_speed_spin(self.robot_vision.get_target_speed())
+        if self.robot_vision.target_view():
+            vertical_error = self.robot_vision.vertical_error
+            self.rpm_speed_spin(vertical_error)
 
     def spin_to_standby_speed(self):
         self.rpm_speed_spin(self.STANDBY_SPEED)
@@ -30,12 +30,12 @@ class Flywheel:
         self.flywheel_motor.set(voltage)
 
     def raw_speed_spin(self, speed):
-        #self.flywheel_motor.changeControlMode(CANTalon.ControlMode.Speed)
-        #self.flywheel_motor.setP(1)
+        # self.flywheel_motor.changeControlMode(CANTalon.ControlMode.Speed)
+        # self.flywheel_motor.setP(1)
         self.flywheel_motor.set(speed)
 
     def rpm_speed_spin(self, speed):
-        #ADD PROPER CONVERSION CONSTANTS
+        # ADD PROPER CONVERSION CONSTANTS
         self.raw_speed_spin(speed)
 
     def increment_speed(self):
@@ -47,10 +47,6 @@ class Flywheel:
         self.currentspeed -= 200
         self.rpm_speed_spin(self.currentspeed)
         print("Current Set Speed: ", self.currentspeed)
-
-        
-
-    
 
 
 class FlywheelSensor(Sensor):
