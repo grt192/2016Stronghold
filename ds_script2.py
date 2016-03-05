aca78de34b23453a39e4437915819afda7805099
Gunn Robotics <Gunn Robotics>
2016-03-05 01:08:45 -0800

    Got driver station script to work on Windows

--- ds_script2.py
+++ ds_script2.py
@@ -1,5 +1,6 @@
 import platform, webbrowser, os, sys
 import multiprocessing as mp
+from multiprocessing import freeze_support
 
 def main():
 	vision_server_url_local = "http://127.0.0.1:5809"
@@ -19,43 +20,55 @@ def main():
 		dash_arg_remote = "java -jar ~/wpilib/tools/SmartDashboard.jar ip roborio-192-frc.local"
 
 	if "Windows" in platform.platform():
-		code_arg_local = b"py C:\Users\GRT Student\Desktop\git\2016Stronghold\py sim"
-		dash_arg_local = b"java -jar C:\Users\GRT Student\wpilib\tools\SmartDashboard.jar ip 127.0.0.1"
-		code_arg_remote = b"py C:\Users\GRT Student\Desktop\git\2016Stronghold\py deploy --skip-tests --no-version-check --nc"
-		dash_arg_remote = b"java -jar C:\Users\GRT Student\wpilib\tools\SmartDashboard.jar ip roborio-192-frc.local"
+		code_arg_local = "py robot.py sim"
+		dash_arg_local = "java -jar SmartDashboard.jar ip 127.0.0.1"
+		code_arg_remote = "py robot.py deploy --skip-tests --no-version-check --nc"
+		dash_arg_remote = "java -jar SmartDashboard.jar ip roborio-192-frc.local"
 
 	if not (len(sys.argv) == 2 or len(sys.argv) == 3):
 		print("No arguments provided!")
 		exit(0)
 	situation = sys.argv[1]
 	if situation == "simv":
+		os.chdir(os.path.normpath("C:/Users/GRT Student/Desktop/git/2016Stronghold/py"))
 		mp.Process(target=os.system, args=(code_arg_local,)).start()
+		os.chdir(os.path.normpath("C:/Users/GRT Student/wpilib/tools"))
 		mp.Process(target=os.system, args=(dash_arg_local,)).start()
 		webbrowser.open(vision_server_url_local, new=1)
 	if situation == "sim":
+		os.chdir(os.path.normpath("C:/Users/GRT Student/Desktop/git/2016Stronghold/py"))
 		mp.Process(target=os.system, args=(code_arg_local,)).start()
+		os.chdir(os.path.normpath("C:/Users/GRT Student/wpilib/tools"))
 		mp.Process(target=os.system, args=(dash_arg_local,)).start()
 	if situation == "dashv":
+		os.chdir(os.path.normpath("C:/Users/GRT Student/wpilib/tools"))
 		mp.Process(target=os.system, args=(dash_arg_remote,)).start()
 		webbrowser.open(camera1_url, new=1)
 		webbrowser.open(camera2_url, new=1)
 		webbrowser.open(vision_server_url_remote, new=1)
 	if situation == "dash":
+		os.chdir(os.path.normpath("C:/Users/GRT Student/wpilib/tools"))
 		mp.Process(target=os.system, args=(dash_arg_remote,)).start()
 		webbrowser.open(camera1_url, new=1)
 		webbrowser.open(camera2_url, new=1)
 	if situation == "deployv":
+		os.chdir(os.path.normpath("C:/Users/GRT Student/Desktop/git/2016Stronghold/py"))
 		mp.Process(target=os.system, args=(code_arg_remote,)).start()
+		os.chdir(os.path.normpath("C:/Users/GRT Student/wpilib/tools"))
 		mp.Process(target=os.system, args=(dash_arg_remote,)).start()
 		webbrowser.open(camera1_url, new=1)
 		webbrowser.open(camera2_url, new=1)
 		webbrowser.open(vision_server_url_remote, new=1)
 	if situation == "deploy":
+		os.chdir(os.path.normpath("C:/Users/GRT Student/Desktop/git/2016Stronghold/py"))
 		mp.Process(target=os.system, args=(code_arg_remote,)).start()
+		os.chdir(os.path.normpath("C:/Users/GRT Student/wpilib/tools"))
 		mp.Process(target=os.system, args=(dash_arg_remote,)).start()
 		webbrowser.open(camera1_url, new=1)
 		webbrowser.open(camera2_url, new=1)
 
 if __name__ == "__main__":
+	if "Windows" in platform.platform():
+		freeze_support()
 	main()
 
