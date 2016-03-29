b08f90b6d1649f31a446cf5cd7ef98c923740a9b
Neelima Valluru <NeelimaValluru@Neelimas-MacBook-Air.local>
2016-03-29 15:51:56 -0700

    changed stuff in drivecontroller

--- py/grt/mechanism/drivecontroller.py
+++ py/grt/mechanism/drivecontroller.py
@@ -9,13 +9,14 @@ class ArcadeDriveController:
     Class for controlling DT in arcade drive mode, with one or two joysticks.
     """
 
-    def __init__(self, dt, l_joystick, record_macro, operation_manager, r_joystick=None):
+    def __init__(self, dt, l_joystick, record_macro, operation_manager, xbox_controller, r_joystick=None):
         """
         Initialize arcade drive controller with a DT and up to two joysticks.
         """
         self.dt = dt
         self.l_joystick = l_joystick
         self.r_joystick = r_joystick
+        self.xbox_controller = xbox_controller
         self.record_macro = record_macro
         operation_manager.shooter.drivecontroller = self
         operation_manager.shooter.dt = self.dt
@@ -56,6 +57,34 @@ class ArcadeDriveController:
         self.manual_control_enabled = True
     def disable_manual_control(self):
         self.manual_control_enabled = False
+
+
+    def _joylistener(self, sensor, state_id, datum):
+        if sensor in (self.l_joystick, self.r_joystick) and state_id in ('x_axis', 'y_axis'):
+            if abs(datum) > .03:
+                if self.manual_control_enabled:
+                    power = self.xbox_controller.r_x_axis
+                    turnval = self.xbox_controller.l_y_axis#self.r_joystick.x_axis if self.r_joystick else self.l_joystick.x_axis
+                    # get turn value from r_joystick if it exists, else get it from l_joystick
+                    self.dt.set_dt_output(power + turnval,
+                                          power - turnval)
+            else:
+                self.dt.set_dt_output(0, 0)
+        elif sensor == self.l_joystick and state_id == 'trigger':
+            if datum:
+                self.dt.downshift()
+                self.dt.enable_protective_measures()
+            else:
+                self.dt.upshift()
+                self.dt.disable_protective_measures()
+
+        elif state_id == "button7":
+            if datum:
+                self.record_macro.start_record()
+        if state_id == "button8":
+            if datum:
+                self.record_macro.stop_record()
+
 
 
 class TankDriveController:
